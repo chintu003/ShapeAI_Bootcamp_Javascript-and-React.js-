5f48d7640499f00d06f7478bed58a8644a4f6828
chintu003 <85408315+chintu003@users.noreply.github.com>
2021-06-26 20:49:38 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Copyright by ShapeAI @ {new Date().getUTCFullYear()}</h1>
+    </footer>
+  );
+}
+export default Footer;
